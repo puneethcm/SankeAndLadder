9f628ff6ef6d9096aa0bf72a647229956fead5b5
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-26 13:34:21 +0530

    UC4

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -4,28 +4,36 @@
     {
         int start = 0,playerPosition = 0; ;
         Console.WriteLine("Start Position:" + start);
-
-        Random random = new Random();
-        int roll = random.Next(0, 7);
-        Console.WriteLine("\nAfter Rolling a dice: " + roll);
-        int Option = random.Next(0, 3);
-        if (Option == 0)
+        while (playerPosition < 100)
         {
-            playerPosition = playerPosition - roll;
-            Console.WriteLine("snake");
-            Console.WriteLine("Player Position is: " + playerPosition);
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine("\nAfter Rolling a dice: " + roll);
+            int Option = random.Next(0, 3);
+            if (Option == 0)
+            {
+                playerPosition = playerPosition - roll;
+                if (playerPosition <= 0)
+                {
+                    playerPosition = 0;
+                    //Console.WriteLine("Player Position is: " + playerPosition);
+                }
+                Console.WriteLine("snake");
+                Console.WriteLine("Player Position is: " + playerPosition);
+                
+            }
+            else if (Option == 1)
+            {
+                playerPosition = playerPosition + roll;
+                Console.WriteLine("ladder");
+                Console.WriteLine("Player Position is: " + playerPosition);
+            }
+            else
+            {
+                Console.WriteLine("no play ");
+                Console.WriteLine("Player Position is: " + playerPosition);
+            }
+            Console.ReadLine();
         }
-        else if (Option == 1)
-        {
-            playerPosition = playerPosition + roll;
-            Console.WriteLine("ladder");
-            Console.WriteLine("Player Position is: " + playerPosition);
-        }
-        else
-        {
-            Console.WriteLine("no play ");
-            Console.WriteLine("Player Position is: " + playerPosition);
-        }
-        Console.ReadLine();
     }
 }

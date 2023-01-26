f2ab10905a447811ca49a069595de8c847f9bb2b
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-26 13:09:44 +0530

    UC3

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -2,14 +2,30 @@
 {
     public static void Main(string[] args)
     {
-        int start = 0, playerPosition=0;
-        Console.WriteLine("\nStart Position:" + start);
+        int start = 0,playerPosition = 0; ;
+        Console.WriteLine("Start Position:" + start);
 
         Random random = new Random();
         int roll = random.Next(0, 7);
-        playerPosition = roll;
-        Console.WriteLine("\nDice rolled number is: " + roll);
-        Console.WriteLine("\nPlayer position is: " + playerPosition);
+        Console.WriteLine("\nAfter Rolling a dice: " + roll);
+        int Option = random.Next(0, 3);
+        if (Option == 0)
+        {
+            playerPosition = playerPosition - roll;
+            Console.WriteLine("snake");
+            Console.WriteLine("Player Position is: " + playerPosition);
+        }
+        else if (Option == 1)
+        {
+            playerPosition = playerPosition + roll;
+            Console.WriteLine("ladder");
+            Console.WriteLine("Player Position is: " + playerPosition);
+        }
+        else
+        {
+            Console.WriteLine("no play ");
+            Console.WriteLine("Player Position is: " + playerPosition);
+        }
         Console.ReadLine();
     }
 }

4e0107ca8c009c111ffc1eef77fc281179634225
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-26 12:56:30 +0530

    UC2

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -2,9 +2,14 @@
 {
     public static void Main(string[] args)
     {
-        int start = 0;
-        Console.WriteLine("Welcome to snake and ladder Game");
+        int start = 0, playerPosition=0;
         Console.WriteLine("\nStart Position:" + start);
+
+        Random random = new Random();
+        int roll = random.Next(0, 7);
+        playerPosition = roll;
+        Console.WriteLine("\nDice rolled number is: " + roll);
+        Console.WriteLine("\nPlayer position is: " + playerPosition);
         Console.ReadLine();
     }
 }

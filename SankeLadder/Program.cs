394f780c777e2ffde77bec53d7428c5d70462afe
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-26 12:45:13 +0530

    UC1 snake

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -3,7 +3,7 @@
     public static void Main(string[] args)
     {
         int start = 0;
-        Console.WriteLine("Welcome to snake and ladder Game");
+        //Console.WriteLine("Welcome to snake and ladder Game");
         Console.WriteLine("\nStart Position:" + start);
         Console.ReadLine();
     }

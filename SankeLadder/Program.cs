77e60408ecbc499c04064f616bd485c82e2759ec
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-27 10:04:56 +0530

    modified

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -5,7 +5,7 @@
         int start = 0,playerPosition1 = 0, diceCount=0, TotalDice=0, playerPosition2 = 0;
         Console.WriteLine("\nWelcome to Sanke And Ladder Game");
         Console.WriteLine("\nPress enter to start the game");
-        Console.WriteLine("Start Position:" + start);
+        Console.WriteLine("Start Position: " + start);
 
         while (playerPosition1 < 100 && playerPosition2 < 100)
         {
@@ -17,35 +17,35 @@
                     Random random1 = new Random();
                     int roll = random1.Next(1, 7);
                     Console.WriteLine("\nDice is Rolled: " + roll);
-                    int Option = random1.Next(1, 2);
-                    if (Option == 0)
-                    {
-                        playerPosition1 = playerPosition1 - roll;
-                        if (playerPosition1 <= 0)
-                        {
-                            playerPosition1 = 0;
-                        }
-                        Console.WriteLine("snake");
-                        Console.WriteLine("Player1 Position is: " + playerPosition1);
-                        diceCount++;
-                    }
-                    else if (Option == 1)
-                    {
-                        playerPosition1 = playerPosition1 + roll;
-                        if (playerPosition1 > 100)
+                    int Option = random1.Next(0, 3);
+                        if (Option == 0)
                         {
                             playerPosition1 = playerPosition1 - roll;
+                            if (playerPosition1 <= 0)
+                            {
+                                playerPosition1 = 0;
+                            }
+                            Console.WriteLine("snake");
+                            Console.WriteLine("Player1 Position is: " + playerPosition1);
+                            diceCount++;
                         }
-                        Console.WriteLine("ladder");
-                        Console.WriteLine("Player1 Position is: " + playerPosition1);
-                        diceCount++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("no play ");
-                        Console.WriteLine("Player1 Position is: " + playerPosition1);
-                        diceCount++;
-                    }
+                        else if (Option == 1)
+                        {
+                            playerPosition1 = playerPosition1 + roll;
+                            if (playerPosition1 > 100)
+                            {
+                                playerPosition1 = playerPosition1 - roll;
+                            }
+                            Console.WriteLine("ladder");
+                            Console.WriteLine("Player1 Position is: " + playerPosition1);
+                            diceCount++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("no play ");
+                            Console.WriteLine("Player1 Position is: " + playerPosition1);
+                            diceCount++;
+                        }
 
                     if (playerPosition1 == 100)
                     {
@@ -58,7 +58,7 @@
                     Random random2 = new Random();
                     int roll2 = random2.Next(1, 7);
                     Console.WriteLine("\nDice is Rolled: " + roll2);
-                    int Option2 = random2.Next(1, 2);
+                    int Option2 = random2.Next(0, 3);
                     if (Option2 == 0)
                     {
                         playerPosition2 = playerPosition2 - roll2;

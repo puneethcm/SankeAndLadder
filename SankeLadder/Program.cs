32e3f662e126a76b244b785d879c4a4ec363c42f
puneethcm <puneethkumarcm8050@gmail.com>
2023-04-09 23:49:22 +0530

    Logical

--- SankeLadder/Program.cs
+++ SankeLadder/Program.cs
@@ -1,59 +1,67 @@
+using SankeLadder;
+
 internal class Program
 {
     public static void Main(string[] args)
     {
-        int start = 0, playerPosition = 0, diceCount = 0, totalDiceCount = 0;
-        const int SNAKE = 0, LADDER = 1;
-        Console.WriteLine("Start Position:" + start);
-        while (playerPosition < 100)
-        {
-            Random random = new Random();
-            int roll = random.Next(1, 7);
-            Console.WriteLine("\nDice is Rolled: " + roll);
-            int Option = random.Next(0, 3);
-            diceCount++;
-            switch (Option)
-            {
-                case SNAKE:
-                    playerPosition -= roll;
-                    Console.WriteLine("Snake");
-                    if (playerPosition <= 0)
-                    {
-                        playerPosition = 0;
-                        Console.WriteLine("Player position is: " + playerPosition);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Player position is: " + playerPosition);
-                    }
-                    break;
-                case LADDER:
-                    playerPosition += roll;
-                    Console.WriteLine("Ladder");
-                    if (playerPosition == 100)
-                    {
-                        Console.WriteLine("Player is Won");
-                    }
-                    else if (playerPosition > 100)
-                    {
-                        playerPosition = playerPosition - roll;
-                        Console.WriteLine("Player remains in same position: " + playerPosition);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Player position id : " + playerPosition);
-                    }
-                    break;
+        //int start = 0, playerPosition = 0, diceCount = 0, totalDiceCount = 0;
+        //const int SNAKE = 0, LADDER = 1;
+        //Console.WriteLine("Start Position:" + start);
+        //while (playerPosition < 100)
+        //{
+        //    Random random = new Random();
+        //    int roll = random.Next(1, 7);
+        //    Console.WriteLine("\nDice is Rolled: " + roll);
+        //    int Option = random.Next(0, 3);
+        //    diceCount++;
+        //    switch (Option)
+        //    {
+        //        case SNAKE:
+        //            playerPosition -= roll;
+        //            Console.WriteLine("Snake");
+        //            if (playerPosition <= 0)
+        //            {
+        //                playerPosition = 0;
+        //                Console.WriteLine("Player position is: " + playerPosition);
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Player position is: " + playerPosition);
+        //            }
+        //            break;
+        //        case LADDER:
+        //            playerPosition += roll;
+        //            Console.WriteLine("Ladder");
+        //            if (playerPosition == 100)
+        //            {
+        //                Console.WriteLine("Player is Won");
+        //            }
+        //            else if (playerPosition > 100)
+        //            {
+        //                playerPosition = playerPosition - roll;
+        //                Console.WriteLine("Player remains in same position: " + playerPosition);
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Player position id : " + playerPosition);
+        //            }
+        //            break;
 
-                default:
-                    //playerPosition += roll;
-                    Console.WriteLine("No game");
-                    Console.WriteLine("Player position is: " + playerPosition);
-                    break;
-            }
-        }
-        totalDiceCount += diceCount;
-        Console.WriteLine("Total Dice Count played to win the game: " + totalDiceCount);
+        //        default:
+        //            //playerPosition += roll;
+        //            Console.WriteLine("No game");
+        //            Console.WriteLine("Player position is: " + playerPosition);
+        //            break;
+        //    }
+        //}
+        //totalDiceCount += diceCount;
+        //Console.WriteLine("Total Dice Count played to win the game: " + totalDiceCount);
+
+        Simple simple = new Simple();
+        //simple.SimpleDemo();
+        //Simple.ReverseArray();
+        //Simple.DecSortedList();
+        Simple.Duplicate();
         Console.ReadLine();
     }
 }
---
+++ SankeLadder/Simple.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace SankeLadder
+{
+	public class Simple
+	{
+		public void SimpleDemo()
+		{
+			List<int> list = new List<int>();
+			list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            //string[] array = list.ToArray();
+            //foreach(string i in array)
+            //{
+            //	Console.WriteLine(i);
+            //}
+            //list.Reverse();
+            //foreach (string i in list)
+            //{
+            //	Console.WriteLine(i);
+            //}
+            //int n = list.Count;
+
+            for (int i = list.Count - 1;i>=0;i--)
+            {
+                Console.WriteLine(list[i]);
+            }
+            
+        }
+
+		public static void ReverseArray()
+		{
+			int[] array = { 10, 20, 30 };
+
+			for (int i = array.Length - 1; i >= 0; i--)
+			{
+				Console.Write(array[i] + " ");
+			}
+		}
+
+        public static void DecSortedList()
+        {
+            SortedList<int, int> sort = new SortedList<int, int>();
+            sort.Add(1,10);
+            sort.Add(3, 30);
+            sort.Add(2, 20);
+            sort.Add(4, 40);
+
+            foreach (KeyValuePair<int, int> element in sort)
+            {
+                Console.WriteLine("key: " + element.Key + " and Value: " + element.Value);
+            }
+
+            //for (int i = sort.Count - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine("key: " + sort.Keys[i] + " and Value: " + sort.Values[i]);
+            //}
+        }
+
+        public static void Duplicate()
+        {
+            int[] array = { 10, 2, 3, 2, 4, 5, 6, 6, 5, 7 };
+            var square = array.Select(x => x);
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        if (array[i] == array[j])
+            //        {
+            //            Console.WriteLine(array[j]);
+            //        }
+            //    }
+            //}
+            foreach(int value in square)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
+
